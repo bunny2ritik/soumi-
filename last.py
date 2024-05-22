7925172ae4a17f6e0309ada4633c3332f9e7230c
bunny2ritik <120122959+bunny2ritik@users.noreply.github.com>
2024-05-22 12:33:03 +0530

    last.py

--- last.py
+++ last.py
@@ -4,13 +4,12 @@ import requests
 from textblob import TextBlob
 
 # Hide Streamlit icon, footer, and status widget
-st.markdown("""
 <style>
 #MainMenu {visibility: hidden;}
 footer {visibility: hidden;}
 .viewerBadge_container__1QSob {visibility: hidden;}
 </style>
-""", unsafe_allow_html=True)
+
 
 # Function to decode the complaint ID from the URL query parameters
 def decode_complaint_id_from_url():
